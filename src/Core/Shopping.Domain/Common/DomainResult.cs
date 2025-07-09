60540fc6e594fabd1b4b498803530dfab5d28e14
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-09 13:08:04 +0330

    add change product

---
+++ src/Core/Shopping.Domain/Common/DomainResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Shopping.Domain.Common;
+
+public record DomainResult(bool IsSuccess, string? ErrorMessage)
+{
+    public static readonly DomainResult Success = new DomainResult(false, null);
+    public static DomainResult Failed(string? errorMessage = null) => new DomainResult(false, errorMessage);
+}

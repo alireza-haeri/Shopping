10477c64e3e0bb807de5acf62009ffbda4173a65
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-10 16:41:01 +0330

    init category entity

---
+++ src/Core/Shopping.Domain/Entities/Product/CategoryEntity.cs
@@ -0,0 +1,18 @@
+﻿using Shopping.Domain.Common;
+
+namespace Shopping.Domain.Entities.Product;
+
+public class CategoryEntity : BaseEntity<Guid>
+{
+    private readonly List<ProductEntity> _products = [];
+
+    public CategoryEntity(string title, Guid? parentId)
+    {
+        Title = title;
+        ParentId = parentId;
+    }
+
+    public string Title { get; private set; }
+    public Guid? ParentId { get; private set; }
+    public IReadOnlyList<ProductEntity> Products => _products.AsReadOnly();
+}

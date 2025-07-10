6318ae14fe931f216b4efa8d921ec3bd7f9b238b
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-10 17:07:49 +0330

    create identity user entities

---
+++ src/Core/Shopping.Domain/Entities/User/UserRoleEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shopping.Domain.Common;
+
+namespace Shopping.Domain.Entities.User;
+
+public sealed class UserRoleEntity : IdentityUserRole<Guid>,IEntity
+{
+    public UserEntity User { get; set; }
+    public RoleEntity Role { get; set; }
+    
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifyDate { get; set; }
+}

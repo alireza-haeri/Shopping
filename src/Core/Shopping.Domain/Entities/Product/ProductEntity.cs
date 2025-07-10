81f94cec159d4b4acfa92e6351666565fb764004
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-10 16:34:09 +0330

    add log value object

--- src/Core/Shopping.Domain/Entities/Product/ProductEntity.cs
+++ src/Core/Shopping.Domain/Entities/Product/ProductEntity.cs
@@ -11,6 +11,7 @@ public class ProductEntity : BaseEntity<Guid>
     }
 
     private readonly List<ImageValueObject> _images = [];
+    private readonly List<LogValueObject> _changeLogs = [];
     public string Title { get; private set; } = null!;
     public string Description { get; private set; } = null!;
     public decimal Price { get; private set; }
@@ -19,7 +20,8 @@ public class ProductEntity : BaseEntity<Guid>
     public Guid UserId { get; private set; }
     public Guid CategoryId { get; private set; }
 
-    public IReadOnlyList<ImageValueObject> Images => _images;
+    public IReadOnlyList<ImageValueObject> Images => _images.AsReadOnly();
+    public IReadOnlyList<LogValueObject> ChangeLogs => _changeLogs.AsReadOnly();
 
     public static ProductEntity Create(Guid? id, string title, string description, decimal price, int quantity,
         ProductState state, Guid? userId, Guid? categoryId)
@@ -31,7 +33,7 @@ public class ProductEntity : BaseEntity<Guid>
         Guard.Against.NegativeOrZero(price, message: "Invalid Price");
         Guard.Against.Negative(quantity, message: "Invalid Quantity");
 
-        return new ProductEntity()
+        var product = new ProductEntity()
         {
             Id = id.Value,
             Title = title,
@@ -42,15 +44,21 @@ public class ProductEntity : BaseEntity<Guid>
             UserId = userId.Value,
             CategoryId = categoryId.Value
         };
+
+        product._changeLogs.Add(LogValueObject.Log("Product Created"));
+
+        return product;
     }
 
     public static ProductEntity Create(string title, string description, decimal price, int quantity,
         ProductState state, Guid? userId, Guid? categoryId)
         => Create(Guid.NewGuid(), title, description, price, quantity, state, userId, categoryId);
 
-    public DomainResult ChangeState(ProductState state)
+    public DomainResult ChangeState(ProductState state, string additionalDescription = null)
     {
         State = state;
+        _changeLogs.Add(LogValueObject.Log("Product State Changed", additionalDescription));
+
         return DomainResult.Success;
     }
 
@@ -59,5 +67,20 @@ public class ProductEntity : BaseEntity<Guid>
         Hidden = 0,
         ShowOnly = 1,
         Active = 2
+    }
+
+    public void Edit(string title, string description, int price,int quantity, Guid? categoryId)
+    {
+        Guard.Against.NullOrEmpty(title, message: "Invalid Title");
+        Guard.Against.NullOrEmpty(categoryId, message: "Invalid Category Id");
+        Guard.Against.NegativeOrZero(price, message: "Invalid Price");
+        Guard.Against.Negative(quantity, message: "Invalid Quantity");
+        
+        Title = title;
+        Description = description;
+        Price = price;
+        CategoryId = categoryId.Value;
+        
+        _changeLogs.Add(LogValueObject.Log("Product Edited"));
     }
 }

d7c87cafe685c51786d4d405615fb09fd4d18a28
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-10 19:01:22 +0330

    change identity entities

---
+++ src/Core/Shopping.Domain/Entities/User/RoleEntity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shopping.Domain.Common;
+
+namespace Shopping.Domain.Entities.User;
+
+public sealed class RoleEntity : IdentityRole<Guid>, IEntity
+{
+    public RoleEntity(string name, string displayName)
+        : base(name)
+    {
+        Name = name;
+        DisplayName = displayName;
+    }
+
+    public string DisplayName { get; private set; }
+    public ICollection<UserRoleEntity> UserRoles { get; set; }= [];
+    public ICollection<RoleClaimEntity> RoleClaims { get; set; }= [];
+
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifyDate { get; set; }
+}
---
+++ src/Core/Shopping.Domain/Entities/User/UserTokenEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shopping.Domain.Common;
+
+namespace Shopping.Domain.Entities.User;
+
+public sealed class UserTokenEntity : IdentityUserToken<Guid> , IEntity
+{
+    public UserEntity User { get; set; }
+    
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifyDate { get; set; }
+}

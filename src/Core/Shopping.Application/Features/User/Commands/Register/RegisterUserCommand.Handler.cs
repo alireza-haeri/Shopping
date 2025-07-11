b2b0f31e2e773f0d58bc40937f065cd6fbfc0a05
AliRezaHM <alirezahairi.86@gamil.com>
2025-07-11 15:59:20 +0330

    Create RegisterUserCommand

---
+++ src/Core/Shopping.Application/Features/User/Commands/Register/RegisterUserCommand.Handler.cs
@@ -0,0 +1,30 @@
+﻿using Mediator;
+using Shopping.Application.Common;
+using Shopping.Application.Contracts.User;
+using Shopping.Application.Extensions;
+using Shopping.Domain.Entities.User;
+
+namespace Shopping.Application.Features.User.Commands.Register;
+
+public class RegisterUserCommandHandler(IUserManager userManager)
+    : IRequestHandler<RegisterUserCommand, OperationResult<bool>>
+{
+    public async ValueTask<OperationResult<bool>> Handle(RegisterUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await new RegisterUserCommandValidator().ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+            return OperationResult<bool>.FailureResult(validationResult.Errors.ConvertKeyValuePairs());
+
+        var user = new UserEntity(request.FirstName, request.LastName, request.UserName, request.Email)
+        {
+            PhoneNumber = request.PhoneNumber
+        };
+
+        var userCreateResult = await userManager.PasswordCreateAsync(user, request.Password, cancellationToken);
+        if (userCreateResult.Succeeded)
+            return OperationResult<bool>.SuccessResult(true); //todo Send confirmation email code
+
+        return OperationResult<bool>.FailureResult(userCreateResult.Errors.ConvertToKeyValuePairs());
+    }
+}

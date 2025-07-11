0d33bae46a7da1f71e78a2d40680267d239c08b1
AliRezaHM <alirezahairi.86@gamil.com>
2025-07-11 15:21:01 +0330

    Create UserManage Interface

---
+++ src/Core/Shopping.Application/Contracts/User/IUserManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shopping.Domain.Entities.User;
+
+namespace Shopping.Application.Contracts.User;
+
+public interface IUserManager
+{
+    Task<IdentityResult> CreateAsync(UserEntity user , CancellationToken cancellationToken);
+    Task<UserEntity?> FindByUserNameAsync(string userName, CancellationToken cancellationToken);
+    Task<IdentityResult> PasswordSignInAsync(UserEntity user, string password, bool rememberMe, CancellationToken cancellationToken);
+}

17d032d9d99deb8e8c87b4e9ec995622d0d0869f
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-11 15:59:02 +0330

    Create ApplicationIdentityExtensions.cs

---
+++ src/Core/Shopping.Application/Extensions/ApplicationIdentityExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shopping.Domain.Entities.Product;
+
+namespace Shopping.Application.Extensions;
+
+public static class ApplicationIdentityExtensions
+{
+    public static List<KeyValuePair<string, string>> ConvertToKeyValuePairs(this IEnumerable<IdentityError> errors)
+    {
+        return errors.Select(error => new KeyValuePair<string, string>("GeneralErrors", error.Description)).ToList();
+    }
+}

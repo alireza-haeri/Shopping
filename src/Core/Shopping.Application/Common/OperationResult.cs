470ff724627a40832b5c45f248a99372a7ad162c
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-11 15:20:43 +0330

    Create OperationResult

---
+++ src/Core/Shopping.Application/Common/OperationResult.cs
@@ -0,0 +1,42 @@
+﻿namespace Shopping.Application.Common;
+
+public interface IOperationResult
+{
+    bool IsSuccess { get; set; }
+    bool IsNotFount { get; set; }
+    List<KeyValuePair<string, string>> ErrorMessages { get; set; }
+}
+
+public class OperationResult<TResult> : IOperationResult
+{
+    public TResult? Result { get; set; }
+
+    public bool IsSuccess { get; set; }
+    public bool IsNotFount { get; set; }
+    public List<KeyValuePair<string, string>>? ErrorMessages { get; set; }
+
+    public static OperationResult<TResult> SuccessResult(TResult result)
+    {
+        return new OperationResult<TResult>()
+        {
+            IsSuccess = true,
+            Result = result
+        };
+    }
+
+    public static OperationResult<TResult> FailureResult(string propertyName, string message)
+    {
+        return new OperationResult<TResult>()
+        {
+            ErrorMessages = [new KeyValuePair<string, string>(propertyName, message)]
+        };
+    }
+
+    public static OperationResult<TResult> NotFoundResult()
+    {
+        return new OperationResult<TResult>()
+        {
+            IsNotFount = true
+        };
+    }
+}

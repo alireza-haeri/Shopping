581e70e347a35b738da76fee1e894b6ef8c10e4b
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-11 15:59:20 +0330

    Create RegisterUserCommand

---
+++ src/Core/Shopping.Application/Features/User/Commands/Register/RegisterUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using Mediator;
+using Shopping.Application.Common;
+
+namespace Shopping.Application.Features.User.Commands.Register;
+
+public record RegisterUserCommand(
+    string FirstName,
+    string? LastName,
+    string UserName,
+    string Email,
+    string PhoneNumber,
+    string Password,
+    string RepeatPassword)
+    : IRequest<OperationResult<bool>>;

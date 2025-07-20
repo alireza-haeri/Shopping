959332e14341d6e5c3e80749a9b451f9e4cc7cb9
AliRezaHM <alirezahairi.86@gamil.com>
2025-07-20 10:40:02 +0330

    init DeleteProductCommand

---
+++ src/Core/Shopping.Application/Features/Product/Commands/DeleteProductCommand.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Mediator;
+using Shopping.Application.Common;
+using Shopping.Application.Common.Validation;
+
+namespace Shopping.Application.Features.Product.Commands;
+
+public record DeleteProductCommand(Guid Id)
+    :IRequest<OperationResult<bool>>,IValidatableModel<DeleteProductCommand>
+{
+    public IValidator<DeleteProductCommand> Validator(ValidationModelBase<DeleteProductCommand> validator)
+    {
+        validator.RuleFor(x=>x.Id).NotEmpty().WithMessage("Id is required");
+        
+        return validator;
+    }
+}

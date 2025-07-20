74c594e3ada6b8dc0ea3c8baeb6dd646ef3f2c07
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-20 11:33:48 +0330

    Create Category Entity Configuration

---
+++ src/Infrastructure/Shopping.Infrastructure.Persistence/Configurations/Category/CategoryEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shopping.Domain.Entities.Product;
+
+namespace Shopping.Infrastructure.Persistence.Configurations.Category;
+
+internal class CategoryEntityConfiguration : IEntityTypeConfiguration<CategoryEntity>
+{
+    public void Configure(EntityTypeBuilder<CategoryEntity> builder)
+    {
+        builder.ToTable("Categories", "product");
+        
+        builder.HasKey(c => c.Id);
+
+        builder.HasIndex(c => c.Title);
+        
+        builder.Property(c => c.Title).HasMaxLength(100).IsRequired().IsUnicode();
+        builder.Property(c => c.ParentId).IsRequired(false).IsUnicode(false);
+        
+        builder.HasMany(c=>c.Products).WithOne(c=>c.Category);
+    }
+}

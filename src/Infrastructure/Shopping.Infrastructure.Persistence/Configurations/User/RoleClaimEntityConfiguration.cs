60a422040fed59bcecd8550c70b5c9b9898e6c79
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-22 10:54:05 +0330

    Create User entities configuration

---
+++ src/Infrastructure/Shopping.Infrastructure.Persistence/Configurations/User/RoleClaimEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shopping.Domain.Entities.User;
+
+namespace Shopping.Infrastructure.Persistence.Configurations.User;
+
+internal class RoleClaimEntityConfiguration:IEntityTypeConfiguration<RoleClaimEntity>
+{
+    public void Configure(EntityTypeBuilder<RoleClaimEntity> builder)
+    {
+        builder.Property(c => c.ClaimValue).HasMaxLength(1000);
+        builder.Property(c => c.ClaimType).HasMaxLength(1000);
+
+        builder.ToTable("RoleClaims", "user");
+    }
+}

d8c793a9ce433e93246f336b5194340dcf408c8b
AliRezaHM <alirezahairi.86@gamil.com>
2025-07-25 12:38:07 +0330

    Create PersistenceTest project and write test for repositories with little bug

---
+++ tests/Shopping.Infrastructure.Persistence.Test/PersistenceTestSetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Infrastructure.Persistence.Repositories.Common;
+using Testcontainers.MsSql;
+
+namespace Shopping.Infrastructure.Persistence.Test;
+
+public class PersistenceTestSetup : IAsyncLifetime
+{
+    public UnitOfWork UnitOfWork { get; set; }
+    private readonly MsSqlContainer _msSqlConfiguration = new MsSqlBuilder().Build();
+    
+    public async Task InitializeAsync()
+    {
+        await _msSqlConfiguration.StartAsync();
+        
+        var dbContextOptionsBuilder = new DbContextOptionsBuilder<ShoppingDbContext>()
+            .UseSqlServer(_msSqlConfiguration.GetConnectionString());
+        
+        var dbContext = new ShoppingDbContext(dbContextOptionsBuilder.Options);
+
+        await dbContext.Database.MigrateAsync();
+        
+        UnitOfWork = new UnitOfWork(dbContext);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _msSqlConfiguration.StopAsync();
+        await UnitOfWork.DisposeAsync(); 
+    }
+}
---
+++ tests/Shopping.Infrastructure.Persistence.Test/UnitOfWorkTests.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using Shopping.Domain.Entities.Product;
+using Shopping.Infrastructure.Persistence.Repositories.Common;
+using Xunit.Abstractions;
+
+namespace Shopping.Infrastructure.Persistence.Test;
+
+public class UnitOfWorkTests(PersistenceTestSetup setup,ITestOutputHelper testOutputHelper) : IClassFixture<PersistenceTestSetup>
+{
+    private readonly UnitOfWork _unitOfWork = setup.UnitOfWork;
+
+    [Fact]
+    public async Task Adding_New_Category_Should_Save_To_Db()
+    {
+        var category = new CategoryEntity("title");
+
+        await _unitOfWork.CategoryRepository.CreateAsync(category);
+        await _unitOfWork.CommitAsync();
+        
+        var categories = await _unitOfWork.CategoryRepository.GetAllAsync();
+        categories.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task Getting_Category_By_Id_Should_Be_Success()
+    {
+        var category = new CategoryEntity("title 2");
+        await _unitOfWork.CategoryRepository.CreateAsync(category);
+        await _unitOfWork.CommitAsync();
+        
+        var categoryInDb = await _unitOfWork.CategoryRepository.GetByIdAsync(category.Id);
+        categoryInDb.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task Added_New_Category_Should_Have_DateAdded()
+    {
+        var category = new CategoryEntity("title 2");
+        await _unitOfWork.CategoryRepository.CreateAsync(category);
+        await _unitOfWork.CommitAsync();
+        
+        var categoryInDb = await _unitOfWork.CategoryRepository.GetByIdAsync(category.Id);
+        
+        categoryInDb.Should().NotBeNull();
+        categoryInDb.CreatedDate.Should().BeMoreThan(TimeSpan.MinValue);
+        
+        testOutputHelper.WriteLine($"Current Added Date: {categoryInDb.CreatedDate}");
+    }
+
+    [Fact]
+    public async Task Added_New_Category_Should_Not_Have_ModifiedDate()
+    {
+        var category = new CategoryEntity("title 2");
+        await _unitOfWork.CategoryRepository.CreateAsync(category);
+        await _unitOfWork.CommitAsync();
+        
+        var categoryInDb = await _unitOfWork.CategoryRepository.GetByIdAsync(category.Id);
+        
+        categoryInDb.Should().NotBeNull();
+        categoryInDb.ModifiedDate.Should().Be(null);
+    }
+
+    //todo this method have a problem => please use another method by update category: who used table no traking
+    [Fact]
+    public async Task Update_Category_Should_Have_ModifiedDate()
+    {
+        var category = new CategoryEntity("title 3");
+        await _unitOfWork.CategoryRepository.CreateAsync(category);
+        await _unitOfWork.CommitAsync();
+        
+        var categoryInDb = await _unitOfWork.CategoryRepository.GetByIdAsync(category.Id);
+        
+        categoryInDb!.Edit("title 4");
+        await _unitOfWork.CommitAsync();
+        
+        var editedCategoryInDb = await _unitOfWork.CategoryRepository.GetByIdAsync(category.Id);
+        
+        editedCategoryInDb.Should().NotBeNull();
+        editedCategoryInDb.ModifiedDate.Should().BeMoreThan(TimeSpan.MinValue);
+    }
+}

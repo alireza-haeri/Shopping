4f20e3c19dd64b9f0f224acf0ed17f914ae1669c
AliRezaHM <alirezahairi.86@gamil.com>
2025-07-29 09:16:32 +0330

    Create Api Result Model and Creat ActionFilters

---
+++ ui/Shopping.WebFramework/Filters/NotFoundAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Shopping.WebFramework.Models;
+
+namespace Shopping.WebFramework.Filters;
+
+public class NotFoundAttribute : ResultFilterAttribute
+{
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        switch (context.Result)
+        {
+            case NotFoundObjectResult notFoundObjectResult:
+            {
+                var apiResult = new ApiResult<object>(false, ApiResultStatusCode.NotFound.ToString("G"),
+                    ApiResultStatusCode.NotFound,
+                    notFoundObjectResult.Value!);
+
+                context.Result = new JsonResult(apiResult) { StatusCode = StatusCodes.Status404NotFound };
+                break;
+            }
+            case NotFoundResult notFoundResult:
+            {
+                var apiResult = new ApiResult(false, ApiResultStatusCode.NotFound.ToString("G"),
+                    ApiResultStatusCode.NotFound);
+
+                context.Result = new JsonResult(apiResult) { StatusCode = StatusCodes.Status404NotFound };
+                break;
+            }
+        }
+    }
+}

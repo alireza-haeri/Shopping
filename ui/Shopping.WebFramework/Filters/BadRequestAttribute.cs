93b862d17f444c82998d66909b91c3ea8576a7c3
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-07-29 09:16:32 +0330

    Create Api Result Model and Creat ActionFilters

---
+++ ui/Shopping.WebFramework/Filters/BadRequestAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Shopping.WebFramework.Models;
+
+namespace Shopping.WebFramework.Filters;
+
+public class BadRequestAttribute : ResultFilterAttribute
+{
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        if (context.Result is not BadRequestObjectResult badRequestObjectResult)
+            return;
+
+        var modelState = context.ModelState;
+
+        if (!modelState.IsValid)
+        {
+            var errors = new ValidationProblemDetails(modelState);
+            var validationErrorApiResult = new ApiResult<IDictionary<string, string[]>>(false,
+                ApiResultStatusCode.BadRequest.ToString("G"), ApiResultStatusCode.BadRequest,
+                errors.Errors);
+
+            context.Result = new JsonResult(validationErrorApiResult) { StatusCode = StatusCodes.Status400BadRequest };
+            return;
+        }
+
+        var badRequestApiResult = new ApiResult<object>(false, ApiResultStatusCode.BadRequest.ToString("G"),
+            ApiResultStatusCode.BadRequest,badRequestObjectResult.Value!);
+        
+        context.Result = new JsonResult(badRequestApiResult) { StatusCode = StatusCodes.Status400BadRequest };
+    }
+}
---
+++ ui/Shopping.WebFramework/Filters/OkResultAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Shopping.WebFramework.Models;
+
+namespace Shopping.WebFramework.Filters;
+
+public class OkResultAttribute : ResultFilterAttribute
+{
+    public override void OnResultExecuting(ResultExecutingContext context)
+    {
+        switch (context.Result)
+        {
+            case OkObjectResult okObjectResult:
+            {
+                var apiResult = new ApiResult<object>(true, ApiResultStatusCode.Ok.ToString("G"),
+                    ApiResultStatusCode.Ok, okObjectResult.Value!);
+                context.Result = new JsonResult(apiResult) { StatusCode = StatusCodes.Status200OK };
+                break;
+            }
+            case OkResult okResult:
+            {
+                var apiResult = new ApiResult(true, ApiResultStatusCode.Ok.ToString("G"),
+                    ApiResultStatusCode.Ok);
+                context.Result = new JsonResult(apiResult) { StatusCode = StatusCodes.Status200OK };
+                break;
+            }
+            default: return;
+        }
+    }
+}

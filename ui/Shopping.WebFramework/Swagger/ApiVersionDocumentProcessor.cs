782507c0bfce1e37bc46a56aeae1c6a99300ac5f
AliRezaHM <alirezahairi.86@gamil.com>
2025-07-29 10:50:06 +0330

    Config Swagger

---
+++ ui/Shopping.WebFramework/Swagger/ApiVersionDocumentProcessor.cs
@@ -0,0 +1,24 @@
+﻿using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using Shopping.WebFramework.Extensions;
+
+namespace Shopping.WebFramework.Swagger;
+
+public class ApiVersionDocumentProcessor:IDocumentProcessor
+{
+    public void Process(DocumentProcessorContext context)
+    {
+        var version = context.Document.Info.Version;
+
+
+        var pathsToRemove = context.Document.Paths
+            .Where(pathItem => !RegExHelpers.MatchesApiVersion(version, pathItem.Key))
+            .Select(c => c.Key)
+            .ToList();
+
+        foreach (var pathToRemove in pathsToRemove)
+        {
+            context.Document.Paths.Remove(pathToRemove);
+        }
+    }
+}
